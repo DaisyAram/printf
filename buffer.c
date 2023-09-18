3c0952fd1418a5bde5dad113b7044b18c6abe966
DaisyAram <daisyarama03@gmail.com>
2023-09-18 12:48:51 -0700

    printf

---
+++ buffer.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+printf
+{
+	char buffer[BUFF_SIZE];
+	int buffer_index = 0;
+
+	if (format == NULL)
+	return (-1);
+	else if (format = '\0')
+	break;
+
+	if(format != '%')
+	buffer[buffer_index] = format;
+
+	if(format == '%')
+	format ++;
+
+	else if (format++ == c || d || s || i)
+	char buffer[BUFF_SIZE];
+	print_buffer(buffer,
