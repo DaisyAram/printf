5bb0aa5f5248bf6394a9c999d72e9203b1ae541b
DaisyAram <daisyarama03@gmail.com>
2023-09-18 04:50:02 -0700

    %,s and c

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include "main.h"
-
+/**
+ * entry - check code
+ *
+ * Return: 0 Always
+ */ 
 int main(void)
 {
-	printf("Our first simple statement\n");
 	_printf("Our first simple statement\n");
+	_printf("Our first simple statement\n");
+	_printf("%c\n", 'a');
+	_printf("%s\n", "printf");
+	_printf("%%\n");
 
 	return (0);
 }
-
--- printf.c
+++ printf.c
@@ -14,19 +14,28 @@ int _printf(const char *format, ...)
 	return (-1);
 
 	va_start(arg, format);
+	/** when the format is not zero, while loop is incoporated */
 	while (*format != '\0')
 	{
+	/** if the format does not have a placeholder "%"*/
 	if (*format != '%')
 	{
 	write(1, format, 1);
+	print_chars++;
 	}
-	format++;
-	}
-	if (*format == '\0')
+	else
 	{
-	return (0);
-	}
+	format++;
+	/** format equals to zero exit */
+	if (*format == '\0')
+	break;
+	/** when the format has placeholder "%" */
 
+	if (*format == '%')
+	{
+	write(1, "%%", 1);
+	print_chars++;
+	}
 	/**when the format is a character */
 	else if (*format == 'c')
 	{
@@ -34,24 +43,23 @@ int _printf(const char *format, ...)
 	write(1, &c, 1);
 	print_chars++;
 	}
-
+	
 	/**when the format is a string */
 	else if (*format == 's')
 	{
 	char *str = va_arg(arg, char*);
 	int str_len = 0;
 
+	/** string length */
 	while (str[str_len] != '\0')
 	str_len++;
 
 	write(1, str, str_len);
 	print_chars += str_len;
 	}
-	
-	/** when the format is %% */
-	if (*format == '%')
-	{
-	write(1, "%%", 1);
+	}
+	/** the while loop countinuea untill all variables are satisfied */
+	format++;
 	}
 	va_end(arg);
 
