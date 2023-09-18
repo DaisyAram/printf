c932bb99d90aedfb992f4482f25324f386403174
Cyrilwere <CYRILWERE>
2023-09-18 12:20:11 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -4,51 +4,27 @@
 void my_printf(const char *format, ...)
 {
 	va_list args;
+
 	va_start(args, format);
+
 
 	while (*format)
 	{
-	if (*format == '%')
-	{
-	format++; /* Move past the '%' */
-	switch (*format)
-	{
-	case 'd':
-	case 'i':
-	/* Handle 'd' and 'i' specifiers for integers */
-	printf("%d", va_arg(args, int));
-	break;
-	case 'c':
-		/* Handle 'c' specifier for characters */
-		putchar(va_arg(args, int)); /* Use int for character */
-		break;
-	case 's':
-		/* Handle 's' specifier for strings */
-		fputs(va_arg(args, char*), stdout);
-		break;
-	case 'b':
-	/* Handle 'b' specifier for binary representation of integers */
-			{
-		int num = va_arg(args, int);
-		for (int i = sizeof(int) * 8 - 1; i >= 0; i--)
+	if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
 		{
-		putchar((num & (1 << i)) ? '1' : '0');
-		}
-	}
-		break;
-		default:
-			/* Print any character following '%' */
-		putchar('%');
-		putchar(*format);
-		break;
-		}
+	/* Handle 'd' and 'i' specifiers */
+	int value = va_arg(args, int);
+
+	printf("%d", value);
+
+	format += 2; /* Move to the next format specifier */
 	}
 		else
 	{
 	/* Print regular characters */
 	putchar(*format);
+	format++;
 	}
-	format++; /* Move to the next character in the format string */
 	}
 
 	va_end(args);
@@ -56,14 +32,8 @@ void my_printf(const char *format, ...)
 
 int main(0)
 {
-	char ch = 'A';
-	char str[] = "Hello, World!";
 	int num = 42;
 
-	my_printf("Character: %c\n", ch);
-	my_printf("String: %s\n", str);
-	my_printf("Integer: %d\n", num);
-	my_printf("Binary: %b\n", num);
-
-	return 0;
+	my_printf("This is a number: %d\n", num);
+	return (0);
 }
