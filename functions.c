7f6c16caf6a821cfe3b42beef60f340f56e0d6c2
DaisyAram <daisyarama03@gmail.com>
2023-09-19 00:27:10 -0700

    functions

---
+++ functions.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+
+	while (*format)
+	{
+	if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		{
+	/* Handle 'd' and 'i' specifiers */
+	int value = va_arg(args, int);
+
+	printf("%d", value);
+
+	format += 2; /* Move to the next format specifier */
+	}
+		else
+	{
+	/* Print regular characters */
+	putchar(*format);
+	format++;
+	}
+	}
+
+	va_end(args);
+}
+
+int main(void)
+{
+	int num = 42;
+
+	_printf("This is a number: %d\n and %i\n", num, num);
+
+	return (0);
+}
