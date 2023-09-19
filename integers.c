c266be7663651d21606a3f7566f51af912976b82
Cyrilwere <CYRILWERE>
2023-09-19 10:49:21 -0700

    integers.c

---
+++ integers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+	if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+	{
+		int value = va_arg(args, int);
+
+		printf("%d", value);
+		format += 2; /* Skip %d or %i */
+	}
+	else
+	{
+	putchar(*format);
+	format++;
+	}
+	}
+
+	va_end(args);
+}
+
+int main(void)
+{
+	int num = 42;
+
+	my_printf("This is a number: %d\n", num);
+	return (0);
+}
