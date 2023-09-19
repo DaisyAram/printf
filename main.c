0535a7cfe30c6e5ace1c92e835d9ea138f4e49af
DaisyAram <daisyarama03@gmail.com>
2023-09-19 00:24:22 -0700

    printf

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Entry - check the code
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char c = 'b';
+	char *s = "Our first printf task";
+
+	_printf("This is %s\n", s);
+	_printf("Alphabet: %c\n", c);
+	_printf("Our first printf task\n");
+	_printf("%%\n");
+	_printf(" \n");
+
+}
--- printf.c
+++ printf.c
@@ -67,23 +67,3 @@ int _printf(const char *format, ...)
 
 	return (print_chars);
 }
-/** main function to test the code above*/
-/**
- * Entry - check the code
- *
- * Return: 0 Always
- */
-int main(void)
-{
-	char c = 'b';
-	char *s = "Our first printf task";
-
-	_printf("This is %s\n", s);
-	_printf("Alphabet: %c\n", c);
-	_printf("Our first printf task\n");
-	_printf("%%\n");
-	_printf(" \n");
-
-
-	return (0);
-}
